7fee2855a51f962f5a09ed8da89761d796638150
Johan Carlsson <johancn87@gmail.com>
2015-06-08 23:23:11 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,45 @@
+NOT DONE!!!
+
+# koLazyOptions
+
+The Description
+
+## About
+
+A JavaScript library by Johan Carlsson.
+
+See the [project homepage](http://johancn87@gmail.com.github.io/koLazyOptions).
+
+## Installation
+
+Using Bower:
+
+    bower install koLazyOptions
+
+Or grab the [source](https://github.com/johancn87@gmail.com/koLazyOptions/dist/koLazyOptions.js) ([minified](https://github.com/johancn87@gmail.com/koLazyOptions/dist/koLazyOptions.min.js)).
+
+## Usage
+
+Basic usage is as follows:
+
+    koLazyOptions();
+
+For advanced usage, see the documentation.
+
+## Documentation
+
+Start with `docs/MAIN.md`.
+
+## Contributing
+
+We'll check out your contribution if you:
+
+* Provide a comprehensive suite of tests for your fork.
+* Have a clear and documented rationale for your changes.
+* Package these up in a pull request.
+
+We'll do our best to help you out with any contribution issues you may have.
+
+## License
+
+MIT. See `LICENSE.txt` in this directory.
---
+++ dist/koLazyOptions.js
@@ -0,0 +1,50 @@
+(function() {
+  "use strict";
+
+/* koLazyOptions main */
+
+ko.bindingHandlers.lazyOptions = {
+  init: function(element, valueAccessor, allBindings) {
+    var initialized = false;
+    var focused = ko.observable();
+    
+    var options = ko.computed({
+      read: function() {
+        if (!initialized && !focused()) {
+          var value = ko.unwrap(allBindings.get("value"));
+          var optionsValue = allBindings.get("optionsValue");
+
+          if (optionsValue) {
+            $.each(valueAccessor(), function(i, item) {
+              if (item[optionsValue] == value) {
+                value = item;
+                return false;
+              }
+              return true;
+            });
+          }
+          
+          if (!value) return [];
+
+          return [value];
+        }
+
+        initialized = true;
+        return ko.unwrap(valueAccessor());
+      },
+      disposeWhenNodeIsRemoved: element
+    });
+
+    ko.applyBindingsToNode(element, {
+      hasFocus: focused,
+      options: options,
+      optionsText: allBindings.get("optionsText"),
+      optionsValue: allBindings.get("optionsValue"),
+      optionsCaption: allBindings.get("optionsCaption"),
+      value: allBindings.get("value")
+    });
+  }
+};
+
+
+}());
